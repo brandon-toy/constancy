c76c9689c60dd3f33e103c36cf1f0006dde84343
brandon-toy <brandontoy@uvic.ca>
2024-07-04 00:06:42 -0700

    move to flex-row

--- components/element.tsx
+++ components/element.tsx
@@ -16,48 +16,35 @@ const elements: string[] = [
 
 interface ImageSource {
     imageSource: string
-    count: number
-    setCount: Function
 }
 
 export default function Elements() {
-    const [count, setCount] = useState(0)
-
     return (
-        <div className="flex place-content-center flex-row">
+        <div className="flex flex-col">
             {elements.map((imageSource) => {
-                return (
-                    <Element
-                        count={count}
-                        setCount={setCount}
-                        imageSource={imageSource}
-                    />
-                )
+                return <Element imageSource={imageSource} />
             })}
         </div>
     )
 }
 
 function Element(props: ImageSource) {
+    const [count, setCount] = useState(0)
     return (
-        <div className="flex flex-col pl-2 pr-2">
-            <button
-                className="text-med"
-                onClick={() =>
-                    props.setCount(
-                        props.count == 0 ? props.count : props.count - 1
-                    )
-                }
-            >
-                {props.count}
-            </button>
-            <button onClick={() => props.setCount(props.count + 1)}>
+        <div className="flex flex-row pt-5 pl-5">
+            <button onClick={() => setCount(count + 1)}>
                 <Image
                     width={35}
                     height={35}
                     src={props.imageSource}
                     alt="fire"
                 />
+            </button>
+            <button
+                className="flex text-xl justify-start grow pl-5 pt-1"
+                onClick={() => setCount(count == 0 ? count : count - 1)}
+            >
+                {count}
             </button>
         </div>
     )
