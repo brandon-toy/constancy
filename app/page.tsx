03fddf5bc3d1fc4a73e25c4171ce1bdfeee67748
Brandon Toy <46540226+brandon-toy@users.noreply.github.com>
2024-07-04 00:48:00 -0700

    Day 2
    * Element row weeeee
    * Prettyyyyy and shared state :O
    * move to flex-row
    * Created reset button
    * DOES ANYONE KNOW A BETTER FUCKING WAY TO RESET STATE LOL

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import Element from "@/components/element";
-import Energy from "@/components/energy";
+'use client'
+
+import Element from '@/components/element'
+import Energy from '@/components/energy'
+import ResetButton from '@/components/resetbutton'
+import { useState } from 'react'
+import { createContext } from 'vm'
 
 export default function Home() {
-  return (
-    <main>
-      <Energy />
-      <Element />
-    </main>
-  );
+    const [reset, setReset] = useState(false)
+
+    return (
+        <main>
+            <Energy />
+            <Element reset={reset} />
+            <ResetButton reset={reset} setReset={setReset} />
+        </main>
+    )
 }
--- components/element.tsx
+++ components/element.tsx
@@ -1,53 +1,59 @@
-"use client"
+'use client'
 
-import Image from "next/image";
-import { useState } from "react"
-
-
-
-
+import Image from 'next/image'
+import { useContext, useState } from 'react'
 
 const elements: string[] = [
-	"/elements/air.png",
-	"/elements/animal.png",
-	"/elements/earth.png",
-	"/elements/fire.png",
-	"/elements/moon.png",
-	"/elements/plant.png",
-	"/elements/sun.png",
-	"/elements/water.png",
+    '/elements/sun.png',
+    '/elements/moon.png',
+    '/elements/fire.png',
+    '/elements/air.png',
+    '/elements/water.png',
+    '/elements/earth.png',
+    '/elements/plant.png',
+    '/elements/animal.png',
 ]
 
 interface ImageSource {
-	imageSource: string
+    imageSource: string
 }
 
+type ElementsProps = {
+    reset: boolean
+}
 
-
-export default function Elements() {
-	const [count, setCount] = useState(0);
-	return (
-		<div className="flex items-center flex-col ">
-			{elements.map((imageSource) => {
-
-				return <Element imageSource={imageSource} />
-			})
-			}
-		</div>
-	)
+export default function Elements(props: ElementsProps) {
+    return (
+        <div className="flex flex-col">
+            {props.reset
+                ? elements.map((imageSource: string, x) => {
+                      return <Element key={1 + x} imageSource={imageSource} />
+                  })
+                : elements.map((imageSource: string, x) => {
+                      return <Element key={-1 * x} imageSource={imageSource} />
+                  })}
+        </div>
+    )
 }
 
 function Element(props: ImageSource) {
-
-	const [count, setCount] = useState(0);
-	return (
-		<div className="flex items-center flex-col ">
-			<button onClick={() => setCount(count == 0 ? count : count - 1)}>
-				{count}
-			</button>
-			<button onClick={() => setCount(count + 1)}>
-				<Image width={50} height={50} src={props.imageSource} alt="fire" />
-			</button>
-		</div>
-	)
+    const [count, setCount] = useState(0)
+    return (
+        <div className="flex flex-row pt-5 pl-5">
+            <button onClick={() => setCount(count + 1)}>
+                <Image
+                    width={35}
+                    height={35}
+                    src={props.imageSource}
+                    alt="fire"
+                />
+            </button>
+            <button
+                className="flex text-xl justify-start grow pl-5 pt-1"
+                onClick={() => setCount(count == 0 ? count : count - 1)}
+            >
+                {count}
+            </button>
+        </div>
+    )
 }
