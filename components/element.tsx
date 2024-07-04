971c6a6b57986e51a7fd775ba4e52a1676e3d12f
brandon-toy <brandontoy@uvic.ca>
2024-07-03 22:58:54 -0700

    Element row weeeee

--- components/element.tsx
+++ components/element.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 
 const elements: string[] = [
     '/elements/sun.png',
