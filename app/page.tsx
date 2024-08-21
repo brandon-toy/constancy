20057fd2ca3687c3d2cc0d7a53e757a439408cd0
Brandon Toy <bghtoy@amazon.com>
2024-08-20 23:14:50 -0700

    gs

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,14 @@
 import Element from '@/components/element'
 import Energy from '@/components/energy'
 import ResetButton from '@/components/resetbutton'
-import { useState } from 'react'
 import StoreProvider from './StoreProvider'
 
 export default function Home() {
-    const [reset, setReset] = useState(false)
-
     return (
         <StoreProvider>
-            <div>
-                <Energy />
-                <Element />
-                <ResetButton />
-            </div>
+            <Energy />
+            <Element />
+            <ResetButton />
         </StoreProvider>
     )
 }
